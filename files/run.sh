4e5102f9460a4f67e58b38e050aeb0bf86fd771a
Vivek Lanjekar <vivek.lanjekar@gmail.com>
2020-04-27 01:56:46 +0530

    Update run.sh

--- files/run.sh
+++ files/run.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-
 if [ -z $PROXY_PASS ]; then
   echo >&2 "PROXY_PASS must be set"
   exit 1
@@ -10,8 +9,6 @@ fi
 
 
 sed \
-  -e "s/##CLIENT_MAX_BODY_SIZE##/$CLIENT_MAX_BODY_SIZE/g" \
-  -e "s/##PROXY_READ_TIMEOUT##/$PROXY_READ_TIMEOUT/g" \
   -e "s/##WORKER_PROCESSES##/$WORKER_PROCESSES/g" \
   -e "s/##SERVER_NAME##/$SERVER_NAME/g" \
   -e "s/##PORT##/$PORT/g" \
